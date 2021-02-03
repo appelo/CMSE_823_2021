bcc17a86c29782899d52cf135217c0b185649904
Daniel Appelo <appeloda@msu.edu>
2021-02-03 11:47:15 -0500

    small fortran example of Gram-Schmidt in double precision

--- easy_fortran/cgs.f90
+++ easy_fortran/cgs.f90
@@ -1,6 +1,6 @@
 module type_defs
   integer, parameter:: sp = kind(1.0),&
-       dp = selected_real_kind(4*precision(1.0_sp)),&
+       dp = selected_real_kind(2*precision(1.0_sp)),&
        qp = selected_real_kind(4*precision(1.0_sp))
 end module type_defs
 
